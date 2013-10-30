56429e745568c5163ab7b49e488c48819543f6cf
John Ford <john@johnford.info>
2013-10-30 11:30:53 -0700

    add claiming and object materialization

--- test/queue_test.js
+++ test/queue_test.js
@@ -3,7 +3,8 @@
 var redis = require('redis'),
     debug = require('debug')('queueTest'),
     rb = require('../lib/refried_beans'),
-    queue = require('../lib/queue.js');
+    queue = require('../lib/queue.js'),
+    uuid = require('uuid');
 
 suite('enqueue', function() {
   // This suite is missing tests for the API returning
@@ -177,5 +178,160 @@ suite('enqueue', function() {
         done();
       });
   });
+});
+
+suite('materialize tasks', function() {
+  var r;
+
+  setup(function() {
+    r = redis.createClient();
+    r.flushall();
+  });
+
+  teardown(function() {
+    r.end();
+  });
+
+  var id = uuid.v4(),
+      optId = uuid.v4(),
+      module = 'module.js',
+      timeout = 2000,
+      options = { num: 1, string: 'test-string' };
+
+  suite('without options', function() {
+    var testObj;
+
+    setup(function(done) {
+      testObj = {
+        module: module,
+        timeout: timeout,
+        options: rb.NO_OPTIONS
+      };
+      r.hmset(id, testObj, function(err, res) {
+        assert.ok(res === 'OK');
+        done(err);
+      });
+    });
+
+    test('task with no options', function(done) {
+      queue.materializeTask(id, function(err, task) {
+        assert.ok(testObj.module, task.module);
+        assert.ok(testObj.timeout, task.timeout);
+        assert.ok({}, task.options);
+        done(err);
+      });
+    });
+
+    test('task with invalid options', function(done) {
+      r.hdel(id, 'options', function(err, res) {
+        assert.equal(res, 1);
+        if (err) done(err);
+        queue.materializeTask(id, function(err, task) {
+          assert.ok(err);
+          assert.ok(!task);
+          done();
+        });
+      });
+    });
+  });
+
+  suite('options', function() {
+    var testObj;
+    var testOpt;
+
+    setup(function(done) {
+      testObj = {
+        module: module,
+        timeout: timeout,
+        options: optId
+      };
+      testOpt = options;
+
+      r.hmset(id, testObj, function(err, res) {
+        assert.ok(res === 'OK');
+        r.hmset(optId, options, function(err, res) {
+          assert.ok(res === 'OK');
+          done(err);
+        });
+      });
+    });
+
+    test('task with real options', function(done) {
+      queue.materializeTask(id, function(err, task) {
+        assert.ifError(err);
+        assert.equal(task.module, module);
+        assert.equal(task.timeout, timeout);
+        assert.deepEqual(task.options, options);
+        done(err);
+      });
+    });
+
+  });
+
 
 });
+
+suite('claiming', function() {
+  var r; // For our redis client obj
+  var task = {
+    module: 'testing.js',
+    timeout: 2000,
+    options: {
+      num: 1,
+      string: 'string'
+    }
+  };
+
+  setup(function(done) {
+    r = redis.createClient();
+
+    function enqueueOne(err) {
+      r.flushall();
+      queue.enqueue(task.module, task.timeout, task.options, function(err) {
+        assert.ifError(err);
+        enqueueTwo();
+      });
+    }
+
+    function enqueueTwo(err) {
+      queue.enqueue(task.module, task.timeout, task.options, function(err) {
+        done(err);
+      });
+    }
+
+    queue.enqueue(task.module, task.timeout, task.options, enqueueOne);
+  });
+
+  teardown(function() {
+    r.end();
+  });
+
+  test('claiming object removes from pending', function(done) {
+    queue.claim(function(err) {
+      assert.ifError(err);
+      r.llen(rb.PENDING_QUEUE, function(err, length) {
+        assert.equal(1, length);
+        done(err);
+      });
+    });
+  });
+
+  test('claiming object inserts to in progress', function(done) {
+    queue.claim(function(err) {
+      assert.ifError(err);
+      r.llen(rb.INPROGRESS_QUEUE, function(err, length) {
+        assert.equal(1, length);
+        done(err);
+      });
+    });
+  });
+
+  test('claiming returns the correct object', function(done) {
+    queue.claim(function(err, claimedTask) {
+      assert.deepEqual(task, claimedTask);
+      done(err);
+    });
+  });
+
+ });
+
